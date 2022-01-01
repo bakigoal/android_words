1c76095f69313d5669eed17612ae9cb88d1f6e32
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-01 12:01:34 +0500

    add menu to switch layout

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.wordsapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.adapter.LetterAdapter
 import com.example.wordsapp.databinding.ActivityMainBinding
@@ -12,6 +16,7 @@ import com.example.wordsapp.databinding.ActivityMainBinding
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private var isLinearLayout = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +25,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         recyclerView = binding.recyclerView
-        // Sets the LinearLayoutManager of the recyclerview
-        recyclerView.layoutManager = GridLayoutManager(this, 4)
+        choseLayout()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        val buttonMenu = menu?.findItem(R.id.action_switch_layout)
+        setIcon(buttonMenu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayout = !isLinearLayout
+                choseLayout()
+                setIcon(item)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun choseLayout() {
+        recyclerView.layoutManager = if (isLinearLayout) {
+            LinearLayoutManager(this)
+        } else {
+            GridLayoutManager(this, 4)
+        }
+
         recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun setIcon(menuItem: MenuItem?) {
+        menuItem?.icon = if (isLinearLayout) {
+            ContextCompat.getDrawable(this, R.drawable.ic_view_list)
+        } else {
+            ContextCompat.getDrawable(this, R.drawable.ic_view_list)
+        }
     }
 
 }
