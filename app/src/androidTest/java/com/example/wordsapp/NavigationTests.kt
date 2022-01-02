bb4c083370d249693e558dfc089f9d0b15fcf9c8
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-02 22:15:29 +0500

    add navigation test

--- app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -1,6 +1,10 @@
 package com.example.wordsapp
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -10,6 +14,8 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.wordsapp.fragment.LetterListFragment
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,6 +34,26 @@ class NavigationTests {
                     .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click())
             )
         onView(withText("Words That Start With C")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navigate_to_words_nav_component() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        val lettersListFragmentScenario =
+            launchFragmentInContainer<LetterListFragment>(themeResId = R.style.Theme_Words)
+
+        lettersListFragmentScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.recycler_view))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click())
+            )
+
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
     }
 
 }
