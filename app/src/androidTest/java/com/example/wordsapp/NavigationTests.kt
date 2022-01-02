a34bfc20b912159e5d69f1135f3b2f96c2fd7641
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-02 12:20:41 +0500

    add that letter c is passed to detail activity

--- app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -3,8 +3,11 @@ package com.example.wordsapp
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -24,6 +27,7 @@ class NavigationTests {
                 RecyclerViewActions
                     .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click())
             )
+        onView(withText("Words That Start With C")).check(matches(isDisplayed()))
     }
 
 }
